36f0df765933f16a785d1f547bbef519b5573ab2
Doug Scheible <doug_scheible@yahoo.com>
2026-03-23 22:29:12 -0500

    did this work?

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,13 @@
+// app/about.tsx
+import Link from "next/link";
+
 export default function About() {
-  return <h1>About Page</h1>;
+  return (
+    <main>
+      <h1>About Page</h1>
+      <Link href="/">
+        Go to Home 
+      </Link>
+    </main>
+  )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
+// app/page.tsx
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
       <h1>My First Next App</h1>
       <p>This is my homepage</p>
+      <Link href="/about">
+        Go to About
+      </Link>
     </main>
   );
 }
