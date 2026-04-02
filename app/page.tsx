2a99c673f4299fffbd2cc7c29e87484a67ddd499
Doug Scheible <doug_scheible@yahoo.com>
2026-04-01 23:04:24 -0500

    added logo on header

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,37 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <header style={styles.header}>
+      <div style={styles.content}>
+        <Image
+          src="/images/icon-cold-hand-capper.png"
+          alt="My First Next App"
+          width={200}
+          height={80}
+        />
+        <p>This is my homepage</p>
+        <Link href="/about">Go to About</Link>
+      </div>
+    </header>
+  );
+}
+
+const styles = {
+  header: {
+    backgroundColor: "blue",
+    color: "white",
+    width: "100%",
+    padding: "20px 0",
+    display: "flex",
+    justifyContent: "center",
+  },
+  content: {
+    textAlign: "center",
+  },
+};
+/*
 // app/page.tsx
 import Link from "next/link";
 
@@ -12,3 +46,4 @@ export default function Home() {
     </main>
   );
 }
+*/
